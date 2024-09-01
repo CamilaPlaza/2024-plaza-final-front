5332f8452194e8fe7f84dfa06ace94aba37e1e59
CamilaPlaza12 <camilaplaza12@uca.edu.ar>
2024-09-01 01:28:39 -0300

    03F - Development of the Login Interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./common/home/home.component";
+import { HeaderComponent } from "./common/header/header.component";
+import { FooterComponent } from "./common/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, HomeComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'C&v-Bar';
 }
--- src/app/common/home/home.component.ts
+++ src/app/common/home/home.component.ts
@@ -1,13 +1,28 @@
+
 import { Component } from '@angular/core';
-import { ButtonModule } from 'primeng/button'; 
+import { FormsModule } from '@angular/forms';
+import { PasswordModule } from 'primeng/password';
+import { DividerModule } from 'primeng/divider';
+import { InputTextModule } from 'primeng/inputtext';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { ButtonModule } from 'primeng/button';
+import { InputIconModule } from 'primeng/inputicon';
+import { IconFieldModule } from 'primeng/iconfield';
+
 
 @Component({
   selector: 'app-home',
-  standalone: true,
-  imports: [ButtonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css'],
+  standalone: true,
+  imports: [FormsModule, DividerModule, PasswordModule, InputIconModule, IconFieldModule, InputTextModule, FloatLabelModule, ButtonModule] 
 })
 export class HomeComponent {
+  username: string = '';
+  password: string = '';
 
+  onLogin() {
+    console.log('Username:', this.username);
+    console.log('Password:', this.password);
+  }
 }

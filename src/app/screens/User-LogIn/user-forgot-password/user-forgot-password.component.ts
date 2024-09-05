d188d15c3bda1bc5dd7505938c2fbc24ca421fd3
Victoria Heine <127904639+mvheine42@users.noreply.github.com>
2024-09-05 19:02:26 -0300

    08F - Test Reset Password

--- src/app/screens/User-LogIn/user-forgot-password/user-forgot-password.component.ts
+++ src/app/screens/User-LogIn/user-forgot-password/user-forgot-password.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UserService } from 'src/app/services/user_service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-forgot-password',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrl: './user-forgot-password.component.css'
 })
 export class UserForgotPasswordComponent {
+  email: string = '';
+  constructor(private userService: UserService, private router: Router) {}
+  async onForgotPassword() {
+    
+    try {
+      const response = await this.userService.resetPassword(this.email);
+      console.log('Reset successful', response);
+      this.router.navigate(['/log-in']);
+
+    } catch (error: any) {
+      console.error('Reset failed', error);
+    }
+  }
 
 }

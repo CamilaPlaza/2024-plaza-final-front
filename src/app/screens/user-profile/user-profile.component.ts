2d8076d61b1ce6339f05bb2c4a6c41b8c62ce78e
CamilaPlaza12 <camilaplaza12@uca.edu.ar>
2024-09-09 23:49:09 -0300

    09F - User Profile

--- src/app/screens/user-profile/user-profile.component.ts
+++ src/app/screens/user-profile/user-profile.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { Product } from 'src/app/models/product';
+import { ConfirmationService } from 'primeng/api';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css'
 })
-export class UserProfileComponent {
+export class UserProfileComponent implements OnInit{
 
+  name: string = 'Camila Plaza';
+  email: string = 'camilaplaza12@gmail.com';
+  birthday: string = '12/02/2001';
+
+  constructor(private confirmationService: ConfirmationService,
+    private router: Router){}
+
+  ngOnInit(): void {}
+
+  async onRegister() {
+    this.confirmationService.confirm({
+      message: 'Are you sure that you want to proceed?',
+      header: 'Confirmation',
+      icon: 'pi pi-exclamation-triangle',
+      accept: async () => {
+        try {
+          console.log('Register successful');
+          this.router.navigate(['/products-view']);
+    
+        } catch (error: any) {
+          console.error('Register failed', error);
+        }
+      }
+    });
+  }
 }

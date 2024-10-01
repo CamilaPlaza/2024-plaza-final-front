ecac78fb2ab2d2a0db798b49863a8bd9d70a2dc8
CamilaPlaza12 <camilaplaza12@uca.edu.ar>
2024-09-30 23:00:19 -0300

    24F - Orders Screen

--- src/app/screens/orders/orders.component.ts
+++ src/app/screens/orders/orders.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
 import { ProductService } from 'src/app/services/product_service';
 import { Router } from '@angular/router'; 
+import { Order } from 'src/app/models/order';
 
 @Component({
   selector: 'app-orders',
@@ -9,41 +10,120 @@ import { Router } from '@angular/router';
   styleUrl: './orders.component.css'
 })
 export class OrdersComponent implements OnInit {
-  products: Product[] = [];  // Arreglo para almacenar los productos
-  categories = [
-    { label: 'Breakfast', value: 'Breakfast' },
-    { label: 'Lunch', value: 'Lunch' },
-    { label: 'Dinner', value: 'Dinner' },
-    { label: 'Drinks', value: 'Drinks' }
+  
+  products: Product[] = [];
+  orders: Order[] = [
+    {
+      id: 1,
+      status: 'Finished',
+      tableNumber: 3,
+      date: '2024-09-30',
+      time: '12:45',
+      total: '25.00',
+      orderItems: [
+        {
+          product: { id: 1, name: 'Hamburguesa Clásica', description: 'Pan, carne, queso, lechuga, tomate', price: '10.00', category: 'Comida', calories: 500 },
+          amount: 2
+        },
+        {
+          product: { id: 2, name: 'Papas Fritas', description: 'Porción mediana', price: '5.00', category: 'Comida', calories: 300 },
+          amount: 1
+        }
+      ]
+    },
+    {
+      id: 2,
+      status: 'In Progress',
+      tableNumber: 5,
+      date: '2024-09-30',
+      time: '13:15',
+      total: '15.00',
+      orderItems: [
+        {
+          product: { id: 3, name: 'Ensalada César', description: 'Lechuga, pollo, crutones, aderezo', price: '8.00', category: 'Comida', calories: 350 },
+          amount: 1
+        },
+        {
+          product: { id: 4, name: 'Limonada', description: 'Vaso grande', price: '4.00', category: 'Bebida', calories: 100 },
+          amount: 1
+        }
+      ]
+    },
+    {
+      id: 3,
+      status: 'In Progress',
+      tableNumber: 1,
+      date: '2024-09-30',
+      time: '13:00',
+      total: '12.00',
+      orderItems: [
+        {
+          product: { id: 5, name: 'Pizza Margherita', description: 'Mozzarella, albahaca, tomate', price: '12.00', category: 'Comida', calories: 700 },
+          amount: 1
+        }
+      ]
+    },
+    {
+      id: 4,
+      status: 'Finished',
+      tableNumber: 8,
+      date: '2024-09-30',
+      time: '13:30',
+      total: '18.00',
+      orderItems: [
+        {
+          product: { id: 6, name: 'Sándwich Vegetariano', description: 'Pan integral, vegetales, queso', price: '7.00', category: 'Comida', calories: 400 },
+          amount: 1
+        },
+        {
+          product: { id: 7, name: 'Jugo de Naranja', description: 'Vaso grande', price: '4.00', category: 'Bebida', calories: 120 },
+          amount: 1
+        },
+        {
+          product: { id: 8, name: 'Brownie', description: 'Porción de brownie con nueces', price: '7.00', category: 'Postre', calories: 450 },
+          amount: 1
+        }
+      ]
+    },
+    {
+      id: 5,
+      status: 'Problem',
+      tableNumber: 2,
+      date: '2024-09-30',
+      time: '12:50',
+      total: '10.00',
+      orderItems: [
+        {
+          product: { id: 9, name: 'Té Helado', description: 'Vaso grande', price: '3.00', category: 'Bebida', calories: 80 },
+          amount: 2
+        },
+        {
+          product: { id: 10, name: 'Bagel con Queso Crema', description: 'Bagel con queso crema', price: '4.00', category: 'Comida', calories: 250 },
+          amount: 1
+        }
+      ]
+    }
   ];
-  displayConfirmDialog: boolean = false;
-  deleteID: number = 0;
+  nroTableOptions: number[] = [];
+  statusOptions: string[] = [];
+  selectedNroTable: number[] = [];
+  selectedStatus: string[] = [];
+
+
   public tableScrollHeight: string='';
 
   constructor(private productService: ProductService, private router: Router) {
+    this.nroTableOptions = [...new Set(this.orders.map(order => order.tableNumber))];
+    this.statusOptions = [...new Set(this.orders.map(order => order.status))];
     
   }
 
   ngOnInit(): void {
     this.loadProducts();
-    // Productos de ejemplo
-    this.products = [
-      new Product('Breakfast Burrito', 'Scrambled eggs with sausage and cheese', '8.99', 'Breakfast', 1),
-      new Product('Chicken Salad', 'Grilled chicken with mixed greens', '10.99', 'Lunch', 2),
-      new Product('Spaghetti Carbonara', 'Pasta with creamy sauce and pancetta', '14.99', 'Dinner', 3),
-      new Product('Margarita Pizza', 'Tomato, mozzarella, and fresh basil pizza', '12.99', 'Dinner', 4),
-      new Product('Club Sandwich', 'Triple-layer sandwich with turkey, bacon, and lettuce', '9.99', 'Lunch', 5),
-      new Product('Caesar Salad', 'Romaine lettuce with Caesar dressing', '7.99', 'Lunch', 6),
-      new Product('Orange Juice', 'Freshly squeezed orange juice', '2.99', 'Drinks', 7),
-      new Product('Cappuccino', 'Rich coffee with steamed milk and foam', '3.99', 'Drinks', 8),
-      new Product('Grilled Cheese Sandwich', 'Classic grilled cheese on toasted bread', '5.49', 'Breakfast', 9),
-      new Product('Chocolate Cake', 'Moist chocolate cake with frosting', '4.99', 'Dessert', 10),
-  ];
-
-  this.setScrollHeight();
-  window.addEventListener('resize', () => {
     this.setScrollHeight();
-  });
+    window.addEventListener('resize', () => {
+      this.setScrollHeight();
+    });
   
   
   }
@@ -55,7 +135,7 @@ export class OrdersComponent implements OnInit {
       this.tableScrollHeight = '400px';
     }
   }
-  // Método para cargar los productos usando el servicio
+
   loadProducts(): void {
     this.productService.getProducts().subscribe({
       next: (data) => {
@@ -72,57 +152,12 @@ export class OrdersComponent implements OnInit {
     });
   }
 
-  onRowEditInit(product: Product) {
-    console.log('Row edit initialized', product);
+  getSeverity(status: string) {
+    // Define aquí la lógica para determinar la severidad según el estado
+    if (status === 'In Progress') return 'success';
+    if (status === 'Problem') return 'danger';
+    if (status === 'Finished') return undefined;
+    return 'info'; // Por defecto
   }
 
-  onRowEditSave(product: Product) {
-    if (parseFloat(product.price) < 0) {
-      console.error('Price cannot be negative');
-      return;
-    }
-    console.log('Row edit saved', product);
-  }
-
-  onRowEditCancel(product: Product, index: number) {
-    console.log('Row edit cancelled', product, index);
-  }
-
-  deleteProduct() {
-    this.products = this.products.filter(product => product.id !== this.deleteID);
-    console.log('Product deleted:', this.deleteID);
-    this.closeConfirmDialog();
-  }
-
-
-  customSort(event: { data: any[]; field: string; order: number }) {
-    event.data.sort((data1, data2) => {
-      let value1 = data1[event.field];
-      let value2 = data2[event.field];
-      let result = null;
-
-      if (value1 == null && value2 != null) result = -1;
-      else if (value1 != null && value2 == null) result = 1;
-      else if (value1 == null && value2 == null) result = 0;
-      else if (typeof value1 === 'string' && typeof value2 === 'string')
-        result = value1.localeCompare(value2);
-      else result = (value1 < value2) ? -1 : (value1 > value2) ? 1 : 0;
-
-      return (event.order * result);
-    });
-  }
-
-  navigateToRegisterProduct(): void {
-    this.router.navigate(['/register-product']); // Asegúrate de que esta ruta esté configurada en tu router
-  }
-
-  showConfirmDialog(id: number) {
-    this.deleteID = id;
-    this.displayConfirmDialog = true;
-  }
-
-  closeConfirmDialog() {
-    this.displayConfirmDialog = false;
-  }
-  
 }

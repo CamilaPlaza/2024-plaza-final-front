c145c015ae6aab3c82e287ce94a485b58b03fdc4
CamilaPlaza12 <camilaplaza12@uca.edu.ar>
2024-10-07 23:29:12 -0300

    Refactor

--- src/app/screens/my-tables/table-finished/table-finished.component.ts
+++ src/app/screens/my-tables/table-finished/table-finished.component.ts
@@ -1,10 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { Order } from 'src/app/models/order';
-import { OrderItem } from 'src/app/models/orderItem';
-import { Product } from 'src/app/models/product';
 import { Table } from 'src/app/models/table';
-import { OrderService } from 'src/app/services/order_service';
-import { ProductService } from 'src/app/services/product_service';
 import { TableService } from 'src/app/services/table_service';
 
 
@@ -16,93 +11,23 @@ import { TableService } from 'src/app/services/table_service';
 export class TableFinishedComponent  implements OnInit {
   @Input() table: Table = new Table('');
   @Output() close = new EventEmitter<void>();
-  actualOrder?: Order; 
-  orderItems: OrderItem[] = [];
-  products : Product[] = [];
-  currentDate: string = '';
-  currentTime: string = '';
-  order: Order = new Order('', 0, '', '', '', []);
   displayConfirmDialog = false;
-  loading: boolean = false;
-  displayCloseTableDialog = false;
 
-  constructor(private productService: ProductService,  private orderService: OrderService, private tableService: TableService) {}
-  ngOnInit() {
-    this.loadProducts();
-    this.getOrderInformation();
-    this.orderItems = this.actualOrder?.orderItems ?? [];
-    this.currentDate = this.actualOrder?.date ?? '';
-    this.currentTime = this.actualOrder?.time ?? '';
-    this.order = this.actualOrder ?? new Order('', 0, '', '', '', []);
-  }
-
-  getOrderInformation() {
-    console.log(this.table)
-    if (this.table.order_id) {
-      this.orderService.getOrderById(this.table.order_id.toString()).subscribe({
-        next: (order) => {
-          this.actualOrder = order; 
-          this.orderItems = this.actualOrder?.orderItems ?? [];
-          this.currentDate = this.actualOrder?.date ?? '';
-          this.currentTime = this.actualOrder?.time ?? '';
-        },
-        error: (err) => {
-          console.error('Error fetching order information:', err);
-        }
-      });
-    }
-  }
-
-  
-  loadProducts(): void {
-    this.productService.getProducts().subscribe({
-      next: (data) => {
-        console.log('Products fetched:', data);
-        if (data && Array.isArray(data.products)) {
-          this.products = data.products;
-        } else {
-          console.error('Unexpected data format:', data);
-        }
-      },
-      error: (err) => {
-        console.error('Error fetching products:', err);
-      }
-      
-    });
-  }
-  
-  getProductById(productId: number | undefined): Product | undefined {
-    if (productId === undefined) {
-      return undefined; 
-    }
-    return this.products.find(product => (product.id) === productId);
-  }
-  
-
-  removeOrderItem(item: OrderItem) {
-    const index = this.orderItems.indexOf(item);
-    if (index > -1) {
-      this.orderItems.splice(index, 1);
-    }
-  }
-
-  calculateTotal() {
-    return this.orderItems.reduce((total, item) => {
-      const product = this.products.find(p => p.id === item.product_id);
-      return product ? total + item.amount * parseFloat(product.price) : total;
-    }, 0);
-  }
+  constructor(private tableService: TableService) {}
+  ngOnInit() {}
 
   cleanTable() {
     this.tableService.cleanTable(this.table).subscribe({
       next: () => {
         console.log('Table cleaned successfully');
         this.closeDialog();
+        location.reload();
       },
       error: (err) => {
         console.error('Error cleaning table:', err);
       }        
     });
+
   }
 
   closeDialog() {
@@ -110,11 +35,4 @@ export class TableFinishedComponent  implements OnInit {
     this.close.emit();  
   }
 
-  showCloseTableDialog() {
-    this.displayCloseTableDialog = true;
-  }
-
-  closeCloseTableDialog() {
-    this.displayCloseTableDialog = false;
-  }
 }

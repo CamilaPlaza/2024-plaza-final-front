da98caa440d173a88b0cba6e29c23d23ac576819
CamilaPlaza12 <camilaplaza12@uca.edu.ar>
2024-10-08 22:14:24 -0300

    Products by category

--- src/app/screens/my-tables/table-free/table-free.component.ts
+++ src/app/screens/my-tables/table-free/table-free.component.ts
@@ -17,18 +17,27 @@ import { Category } from 'src/app/models/category';
 export class TableFreeComponent implements OnInit {
   @Input() table: Table = new Table('');
   @Output() close = new EventEmitter<void>();
+  searchTerm: string = ''; 
+  filteredProducts: Product[] = []; 
   categories: Category[] = [];
   orderItems: OrderItem[] = [];
   loading: boolean = false;
   selectedProduct: Product | null = null;
   selectedAmount: number = 1;
   canAddProduct: boolean = false;
-  products : Product[] = [];
+  products: Product[] = [];
   currentTime: string = '';
   order: Order | undefined;
   currentDate: string = this.formatDate(new Date());
+  
+  selectedCategories: Array<{ id: any, name: string }> = [];
 
-  constructor(private productService: ProductService, private orderService: OrderService, private tableService: TableService,  private categoryService: CategoryService) {}
+  constructor(
+    private productService: ProductService,
+    private orderService: OrderService,
+    private tableService: TableService,
+    private categoryService: CategoryService
+  ) {}
 
   ngOnInit() {
     this.updateCurrentTime();
@@ -36,6 +45,7 @@ export class TableFreeComponent implements OnInit {
     this.loadCategories();
   }
 
+  // Función que carga los productos disponibles
   loadProducts(): void {
     this.productService.getProducts().subscribe({
       next: (data) => {
@@ -58,25 +68,23 @@ export class TableFreeComponent implements OnInit {
     this.currentTime = `${hours}:${minutes}`;
   }
 
-  onProductChange() {
-    this.validateForm();
-  }
-
-  validateForm() {
-    this.canAddProduct = !!this.selectedProduct && this.selectedAmount > 0;
-  }
 
   addOrderItem() {
+    console.log('prod selec: ', this.selectedProduct);
+    
     if (this.selectedProduct && this.selectedAmount > 0) {
       const newItem: OrderItem = {
         product_id: this.selectedProduct.id ?? 0,
         amount: this.selectedAmount
       };
+      console.log('newItem: ', newItem);
+      
       this.orderItems.push(newItem);
       this.resetForm();
     }
   }
 
+  
   removeOrderItem(item: OrderItem) {
     const index = this.orderItems.indexOf(item);
     if (index > -1) {
@@ -84,20 +92,22 @@ export class TableFreeComponent implements OnInit {
     }
   }
 
+  
   resetForm() {
     this.selectedProduct = null;
     this.selectedAmount = 1;
     this.canAddProduct = false;
   }
 
+  // Calcular el total del pedido
   calculateTotal() {
     return this.orderItems.reduce((total, item) => {
-      const product = this.products.find(p => p.id === item.product_id); 
+      const product = this.products.find(p => p.id === item.product_id);
       return product ? total + item.amount * parseFloat(product.price) : total;
     }, 0);
   }
 
-
+  // Crear la orden
   async createOrder() {
     this.loading = true;
     const total = this.calculateTotal();
@@ -108,46 +118,43 @@ export class TableFreeComponent implements OnInit {
       time: this.currentTime,
       total: total.toString(),
       orderItems: this.orderItems
-    }; 
+    };
     try {
       const response = await this.orderService.onRegister(this.order); // Aquí se registra la orden
-      if (response && response.order && response.order_id) {  // Asegúrate de que haya un order_id
-          console.log(this.order); // Esta es la orden antes de la respuesta
-          console.log('Order Register successful', response);
-          
-          // Llama a updateTableAndOrder pasando la orden y el order_id
-          await this.tableService.updateTableAndOrder(response.order, response.order_id); // Cambié aquí
-          this.updateTable(); // Actualizas la tabla después de la asociación
-          this.closeDialog(); // Cierra el diálogo si todo sale bien
+      if (response && response.order && response.order_id) {
+        console.log(this.order); // Esta es la orden antes de la respuesta
+        console.log('Order Register successful', response);
+        
+        await this.tableService.updateTableAndOrder(response.order, response.order_id); // Cambié aquí
+        this.updateTable(); // Actualizas la tabla después de la asociación
+        this.closeDialog(); // Cierra el diálogo si todo sale bien
       } else {
-          console.log('Order registration failed');
+        console.log('Order registration failed');
       }
-  } catch (error: any) {
+    } catch (error: any) {
       console.error('Error durante el registro:', error);
-  } finally{
-    this.loading = false; // Detener el spinner
+    } finally {
+      this.loading = false; // Detener el spinner
+    }
   }
 
-}
-
-
+  // Formatear la fecha
   formatDate(date: Date): string {
     const year = date.getFullYear();
     const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Mes comienza desde 0
     const day = date.getDate().toString().padStart(2, '0');
-    return `${year}-${month}-${day}` // Formato YYYY-MM-DD
+    return `${year}-${month}-${day}`; // Formato YYYY-MM-DD
   }
 
+  // Obtener producto por ID
   getProductById(productId: number): Product | undefined {
     return this.products.find(product => product.id === productId);
   }
-  
 
-  updateTable(){
-    //TO DO: QUE LO UPDATEE
+  // Actualizar la tabla
+  updateTable() {
     this.table.status = 'BUSY';
-    this.table.order_id = this.order?.id;  
-    
+    this.table.order_id = this.order?.id;
     console.log('Table updated', this.table);
   }
 
@@ -160,40 +167,49 @@ export class TableFreeComponent implements OnInit {
     this.close.emit();
   }
 
+  // Cargar las categorías
   loadCategories(): void {
     this.categoryService.getCategories().subscribe({
-        next: (data) => {
-            if (data && Array.isArray(data.categories)) {
-                this.categories = data.categories.map(item => ({
-                    id: item.id,
-                    name: item.name,
-                    type: item.type
-                }));
-            }
-        },
-        error: (err) => {
-            console.error('Error fetching categories:', err);
+      next: (data) => {
+        if (data && Array.isArray(data.categories)) {
+          this.categories = data.categories.map(item => ({
+            id: item.id,
+            name: item.name,
+            type: item.type
+          }));
         }
+      },
+      error: (err) => {
+        console.error('Error fetching categories:', err);
+      }
     });
   }
-  getProductsByCategory(categoryId: string): Promise<Product[]> {
-    return this.categoryService.getProductsByCategory(categoryId)
-      /*.then((data) => {
+
+  // Filtrar productos por categorías seleccionadas
+  filterProductsByCategory() {
+    if (this.selectedCategories.length === 0) {
+      this.filteredProducts = [];
+    } else {
+      const categoryIds = this.selectedCategories.map((category: { id: any; }) => category.id).join(', ');
+      this.getProductsByCategory(categoryIds);
+    }
+  }
+
+  getProductsByCategory(categoryIds: string) {
+    this.categoryService.getProductsByCategory(categoryIds)
+      .then((data) => {
         console.log('Products fetched for category:', data);
         if (data && Array.isArray(data)) {
-          return data; // Retorna toda la data de los productos
+          console.log('prod filtrados: ', data);
+          this.filteredProducts = data;
         } else {
           console.error('Unexpected data format:', data);
-          return []; // En caso de formato inesperado, retornamos un array vacío
+          this.filteredProducts = [];
         }
       })
       .catch((err) => {
         console.error('Error fetching products by category:', err);
-        return []; // En caso de error, retornamos un array vacío
+        this.filteredProducts = []; 
       })
-      .finally(() => {
-        this.loading = false; // Detener el spinner cuando la carga finaliza
-      });*/
-      //SI NO TE FUNCIONA DESCOMENTA ESO
-    }
   }
+}

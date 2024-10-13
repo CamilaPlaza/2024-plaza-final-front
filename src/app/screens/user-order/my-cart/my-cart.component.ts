01930f3347a29c46dfab108cbf5b3dd7e6e276bb
CamilaPlaza12 <camilaplaza12@uca.edu.ar>
2024-10-12 22:21:11 -0300

    29F - User register order

--- src/app/screens/user-order/my-cart/my-cart.component.ts
+++ src/app/screens/user-order/my-cart/my-cart.component.ts
@@ -10,39 +10,35 @@ export class MyCartComponent {
   @Input() orderItems: OrderItem[] = [];
   @Input() isVisible: boolean = false;
 
-   // Método para cerrar el carrito
+   
    closeCart(): void {
-    this.isVisible = false;
-}
-
-// Método para crear una orden (implementar la lógica de creación aquí)
-createOrder(): void {
-    // Lógica para crear la orden
-    console.log('Creating order...', this.orderItems);
-}
-
-// Obtener el precio total
-getTotalPrice(): number {
-    return this.orderItems.reduce((total, item) => total + (item.amount * parseFloat(item.product_price)), 0);
-}
-
-// Método para incrementar la cantidad de un producto
-incrementProduct(productId: number): void {
-    const orderItem = this.orderItems.find(item => item.product_id === productId);
-    if (orderItem) {
-        orderItem.amount += 1;
+        this.isVisible = false;
     }
-}
 
-// Método para decrementar la cantidad de un producto
-decrementProduct(productId: number): void {
-    const orderItem = this.orderItems.find(item => item.product_id === productId);
-    if (orderItem && orderItem.amount > 1) {
-        orderItem.amount -= 1;
-    } else if (orderItem && orderItem.amount === 1) {
-        // Si el amount es 1, podrías optar por eliminarlo o solo reducirlo a 0
+    createOrder(): void {
+        console.log('Creating order...', this.orderItems);
+    }
+
+
+    getTotalPrice(): number {
+        return this.orderItems.reduce((total, item) => total + (item.amount * parseFloat(item.product_price)), 0);
+    }
+
+
+    incrementProduct(productId: number): void {
+        const orderItem = this.orderItems.find(item => item.product_id === productId);
+        if (orderItem) {
+            orderItem.amount += 1;
+        }
+    }
+
+    decrementProduct(productId: number): void {
+        const orderItem = this.orderItems.find(item => item.product_id === productId);
+        if (orderItem && orderItem.amount > 1) {
+            orderItem.amount -= 1;
+        } else if (orderItem && orderItem.amount === 1) {
         this.orderItems = this.orderItems.filter(item => item.product_id !== productId);
+        }
     }
-}
 
 }

29e45043242b9fdf4d127a8b3e487cd304b1dbf8
CamilaPlaza12 <camilaplaza12@uca.edu.ar>
2024-10-31 23:24:50 -0300

    35F - Goals Table

--- src/app/screens/goals/goals.component.ts
+++ src/app/screens/goals/goals.component.ts
@@ -12,12 +12,21 @@ export class GoalsComponent implements OnInit {
     new Goal('Learn TypeScript', 'Complete the TypeScript course on Udemy', 80, 'blue', 'pi pi-graduation-cap', 1),
     new Goal('Read Books', 'Read 12 books this year', 50, 'orange', 'pi pi-book', 3),
     new Goal('Get Fit', 'Exercise at least 4 times a week', 20, 'red', 'pi pi-dumbbell', 4),
+    new Goal('Travel', 'Visit 3 new countries this year', 30, 'purple', 'pi pi-plane', 5),
+    new Goal('Learn TypeScript', 'Complete the TypeScript course on Udemy', 80, 'blue', 'pi pi-graduation-cap', 1),
+    new Goal('Read Books', 'Read 12 books this year', 50, 'orange', 'pi pi-book', 3),
+    new Goal('Get Fit', 'Exercise at least 4 times a week', 20, 'red', 'pi pi-dumbbell', 4),
     new Goal('Travel', 'Visit 3 new countries this year', 30, 'purple', 'pi pi-plane', 5)
   ];
-  
+
+  visibleGoals: Goal[] = [];
+  currentIndex: number = 0;
+  itemsPerPage: number = 4;
   selectedDate: Date = new Date();  
 
-  constructor() { }
+  constructor() { 
+    this.visibleGoals = this.goals.slice(0, 4);
+  }
 
   ngOnInit(): void { }
 
@@ -49,4 +58,24 @@ export class GoalsComponent implements OnInit {
   setLastDayOfMonth(month: number, year: number): void {
     this.selectedDate = new Date(year, month, 0);
   }
+
+  
+  updateVisibleCategories(): void {
+    this.visibleGoals = this.goals.slice(this.currentIndex, this.currentIndex + this.itemsPerPage);
+  }
+
+  nextPage(): void {
+    if (this.currentIndex + this.itemsPerPage < this.goals.length) {
+      this.currentIndex++;
+      this.updateVisibleCategories();
+    }
+  }
+
+  prevPage(): void {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+      this.updateVisibleCategories();
+    }
+  }
+
 }

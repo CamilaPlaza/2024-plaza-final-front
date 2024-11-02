a71d745ed0e7617bf8d1124e57f81855c9c16377
CamilaPlaza12 <camilaplaza12@uca.edu.ar>
2024-11-02 20:34:56 -0300

    35F - Goals Table

--- src/app/models/goal.ts
+++ src/app/models/goal.ts
@@ -5,13 +5,15 @@ export class Goal {
     progress: number;
     color: string;
     icon: string;
+    date: string;
     
-    constructor(title: string, description: string, progress: number, color:string, icon:string, id?:number) {
+    constructor(title: string, description: string, progress: number, color:string, icon:string, date: string, id?:number) {
       this.title = title;
       this.description = description;
       this.progress = progress;
       this.id = id;
       this.color = color;
       this.icon = icon;
+      this.date = date;
     }
 }
--- src/app/screens/goals/goals.component.ts
+++ src/app/screens/goals/goals.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Goal } from 'src/app/models/goal';
 import { CalendarMonthChangeEvent, CalendarYearChangeEvent } from 'primeng/calendar';
+import { ChartModule } from 'primeng/chart';
 
 @Component({
   selector: 'app-goals',
@@ -9,26 +10,78 @@ import { CalendarMonthChangeEvent, CalendarYearChangeEvent } from 'primeng/calen
 })
 export class GoalsComponent implements OnInit {
   goals: Goal[] = [
-    new Goal('Learn TypeScript', 'Complete the TypeScript course on Udemy', 80, 'blue', 'pi pi-graduation-cap', 1),
-    new Goal('Read Books', 'Read 12 books this year', 50, 'orange', 'pi pi-book', 3),
-    new Goal('Get Fit', 'Exercise at least 4 times a week', 20, 'red', 'pi pi-dumbbell', 4),
-    new Goal('Travel', 'Visit 3 new countries this year', 30, 'purple', 'pi pi-plane', 5),
-    new Goal('Learn TypeScript', 'Complete the TypeScript course on Udemy', 80, 'blue', 'pi pi-graduation-cap', 1),
-    new Goal('Read Books', 'Read 12 books this year', 50, 'orange', 'pi pi-book', 3),
-    new Goal('Get Fit', 'Exercise at least 4 times a week', 20, 'red', 'pi pi-dumbbell', 4),
-    new Goal('Travel', 'Visit 3 new countries this year', 30, 'purple', 'pi pi-plane', 5)
+    new Goal('Learn TypeScript', 'Complete the TypeScript course on Udemy', 80, 'blue', 'pi pi-graduation-cap', '01/11/2024', 1),
+    new Goal('Read Books', 'Read 12 books this year  at least please', 50, 'orange', 'pi pi-book', '01/11/2024', 3),
+    new Goal('Get Fit', 'Exercise at least 4 times a week', 20, 'red', 'pi pi-gym', '01/11/2024', 4),
+    new Goal('Travel', 'Visit 3 new countries this year', 30, 'purple', 'pi pi-plane', '01/11/2024', 5),
+    new Goal('Learn Love', 'Complete the TypeScript course on Udemy', 40, 'blue', 'pi pi-love', '01/11/2024', 1)
   ];
-
   visibleGoals: Goal[] = [];
   currentIndex: number = 0;
   itemsPerPage: number = 4;
   selectedDate: Date = new Date();  
+  data: any;
+  options: any;
+  colors: string[] = ['#7f522e', '#b37a3a'];
 
   constructor() { 
     this.visibleGoals = this.goals.slice(0, 4);
   }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    const documentStyle = getComputedStyle(document.documentElement);
+    const textColor = documentStyle.getPropertyValue('--text-color');
+    const textColorSecondary = documentStyle.getPropertyValue('--text-color');
+    const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
+    this.data = {
+      labels: this.goals.map(goal => goal.title),
+      datasets: [
+          {
+              label: 'Progress',
+              backgroundColor: this.goals.map(goal => goal.color),
+              borderColor: this.goals.map(goal => goal.color), 
+              data: this.goals.map(goal => goal.progress)
+          }
+      ]
+    };
+
+    this.options = {
+      indexAxis: 'y',
+      maintainAspectRatio: false,
+      aspectRatio: 1.2,
+      plugins: {
+          legend: {
+              labels: {
+                  color: textColor
+              }
+          }
+      },
+      scales: {
+          x: {
+              max: 100,
+              ticks: {
+                  color: textColorSecondary,
+                  font: {
+                      weight: 500
+                  }
+              },
+              grid: {
+                  color: surfaceBorder,
+                  drawBorder: false
+              }
+          },
+          y: {
+              ticks: {
+                  color: textColorSecondary
+              },
+              grid: {
+                  color: surfaceBorder,
+                  drawBorder: false
+              }
+          }
+      }
+    };
+  }
 
   addNewGoal(): void {
     console.log('Adding new goal');
